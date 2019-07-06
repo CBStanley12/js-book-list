a2bfeac8ef7a2e9cad74107d3164ebd61f4ed5d8
Christopher Stanley <cbstanley12@gmail.com>
2019-07-06 09:15:16 -0600

    feat: app event listeners

--- appes6.js
+++ appes6.js
@@ -7,3 +7,50 @@ class Book {
 }
 
 class UI {}
+
+// Event listener for add book
+document.getElementById('book-form').addEventListener('submit', function(e) {
+	// Get form values
+	const title = document.getElementById('title').value,
+				author = document.getElementById('author').value,
+				isbn = document.getElementById('isbn').value;
+	
+	// Instantiate a book
+	const book = new Book(title, author, isbn);
+
+	// Instantiate UI
+	const ui = new UI();
+
+	// Validate
+	if (title === '' || author === '' || isbn === '') {
+		ui.showAlert('Please fill in all fields', 'error');
+	} else {
+		// Add book to list
+		ui.addBookToList(book);
+		// Add to localStorage
+		Store.addBook(book);
+		// Show success alert
+		ui.showAlert('Book Added!', 'success');
+		// Clear fields
+		ui.clearFields();
+	}
+
+	e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+	// Instantiate uiUI
+	const ui = new UI;
+
+	// Delete book
+	ui.deleteBook(e.target);
+
+	// Remove from localStorage
+	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+	// Show alert
+	ui.showAlert('Book removed!', 'success');
+
+	e.preventDefault();
+});
