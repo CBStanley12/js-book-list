632f764a2ab257128416d53c0848ee9892e9c8a1
Christopher Stanley <cbstanley12@gmail.com>
2019-07-06 09:16:20 -0600

    feat: ui method to add book to list

--- appes6.js
+++ appes6.js
@@ -6,7 +6,24 @@ class Book {
 	}
 }
 
-class UI {}
+class UI {
+	addBookToList(book) {
+		const list = document.getElementById('book-list');
+
+		// Create tr element
+		const row = document.createElement('tr');
+
+		//Insert cols
+		row.innerHTML = `
+			<td>${book.title}</td>
+			<td>${book.author}</td>
+			<td>${book.isbn}</td>
+			<td><a href="#" class="delete">X</a></td>
+		`;
+
+		list.appendChild(row);
+	}
+}
 
 // Event listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
